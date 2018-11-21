f799be632eae292e875814fbd4caf188647406e2
unknown <tarkowr@gmail.com>
2018-11-21 12:00:47 -0500

    Uses Github API to read in updated dates

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# ePortfolio
+# E-Portfolio
 
 **Created By:** Richie Tarkowski 
 
-**Institution:** Northwestern Michigan College
-
-**Date Last Modified:** October 11, 2018
-
-### Purpose of this portfolio
-Showcase my accomplishments and highlight my skills to potential employers.
-
 ### Technologies Used
-HTML5, CSS3, JavaScript, and Minimal Angular.JS
-
-### Special Thanks To
-Google Fonts API and Font Awesome
+HTML5, CSS3, JavaScript, and some Angular.JS
--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,7 @@ const scrollBarWidth = 17;
 
 $(function() {
     let nav = $("#navigation");
+
     let ToggleNavBar = function(){
         var scroll = $(window).scrollTop();
         if(!($(window).innerWidth() + scrollBarWidth <= maxSmallScreen)){
@@ -50,5 +51,42 @@ document.getElementById("nav-bars").addEventListener("click", function(){
         }
     }
 });
+
+window.onload = function() {
+    ApiService("https://api.github.com/users/tarkowr/repos");
+};
+
+function ApiService(url){
+    const eportfolioDate = document.getElementById("eportfolio-date");
+    const cit228Date = document.getElementById("cit228-date");
+    const cit195Date = document.getElementById("cit195-date");
+
+    const Http = new XMLHttpRequest();
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange=(e)=>{
+        if(Http.readyState === 4 && Http.status === 200) {
+            eportfolioDate.innerHTML = GetDateFromGithubApi(ConvertJsonToObject(Http.responseText), "ePortfolio");
+            cit228Date.innerHTML = GetDateFromGithubApi(ConvertJsonToObject(Http.responseText), "CIT-228-Discussion-Forum");
+            cit195Date.innerHTML = GetDateFromGithubApi(ConvertJsonToObject(Http.responseText), "CIT195-Final-Project");
+        }
+    }
+}
+
+function ConvertJsonToObject(json){
+    return JSON.parse(json);
+}
+
+function GetDateFromGithubApi(obj, name){
+    var repo = obj.find(function(element) {
+        return element.name == name;
+    });
+    var repoLastUpdated = new Date(repo.updated_at);
+    var date = repoLastUpdated.getDate();
+    var month = repoLastUpdated.getMonth();
+    var year = repoLastUpdated.getFullYear();
+
+    return  (month + 1) + "-" + date + "-" + year;
+}
 
   
