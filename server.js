a698d3aa3050b74cf3ea21ce9bdc322c59715a7c
Richie Tarkowski <tarkowr@gmail.com>
2020-04-30 15:29:53 -0400

    Changes CSS structure

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var path = require ('path')
 var axios = require('axios')
 var cookieParser = require('cookie-parser')
 var cryptoJS = require('crypto-js')
@@ -32,7 +33,7 @@ var urls = {
 // Set the view engine to ejs
 app.set('view engine', 'ejs')
 app.use(cookieParser())
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(path.join(__dirname + '/static')))
 
 
 // View Routing
